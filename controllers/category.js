6df67b924a3de520bdbd5b924adb4d2f6efe9462
Divyarajsinh01 <divyarajsinhrayjada3@gmail.com>
2024-03-06 15:12:48 +0530

    cahnges done

--- controllers/category.js
+++ controllers/category.js
@@ -5,8 +5,8 @@ exports.addCategory = async (req, res, next) => {
     try {
         const { category_name } = req.body
         if (!category_name) throw new Error("Please provide a Category Name")
-        
-        if(!isNaN(category_name)) throw new Error('category name should be character')
+
+        if (!isNaN(category_name)) throw new Error('category name should be character')
 
         const categorylist = category.build({
             category_name
@@ -40,6 +40,19 @@ exports.allCategory = async (req, res, next) => {
                     }]
                 }]
             })
+
+            categoryData = categoryData.filter(category => {
+                return category.product_detail.length > 0;
+            }); 
+
+            // Filter products with images for each category
+            categoryData = categoryData.map(category => ({
+                ...category.toJSON(),
+                product_detail: category.product_detail.filter(prod => prod.product_images.length > 0)
+            }))
+
+            if(!categoryData.length) throw new Error('there is no category')
+
         } else {
             categoryData = await category.findOne({
                 where: { categoryID },
@@ -54,10 +67,13 @@ exports.allCategory = async (req, res, next) => {
                     }]
                 }]
             })
+
+            categoryData = {
+                ...categoryData.toJSON(),
+                product_detail : categoryData.product_detail.filter(prod => prod.product_images.length > 0)
+            }
+            if (!categoryData) throw new Error("there is no category")
         }
-
-
-        if (!categoryData.length) throw new Error("there is no category")
 
         res.status(200).json({
             status: 200,
