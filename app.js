3d7d5a90a13c059ac4bbae82c7543825c48c7c0f
Divyarajsinh01 <141410024+Divyarajsinh01@users.noreply.github.com>
2024-03-12 14:57:12 +0530

    health endpoint add (#20)

--- app.js
+++ app.js
@@ -9,13 +9,32 @@ const shippingRouter = require('./routers/addShippingAddress')
 const orderRouter = require('./routers/placeOrder')
 const app = express()
 const cors = require('cors')
-require('./config/dbconnect')
+const sequelize = require('./config/dbconnect')
 
 const PORT = process.env.PORT || 3000
 
 app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }));
+
+app.get('/health', async (req, res) => {
+    try {
+  await sequelize.authenticate()
+  console.log('Connection has been established successfully.')
+  await sequelize.sync()
+  console.log('All models synchronized successfully.')
+  res.status(200).json({
+    status: 200,
+    message: 'Connection has been established successfully.'
+  })
+} catch (error) {
+    res.status(400).json({
+        status: 400,
+        message: `Unable to connect to the database: ${error.message}`
+    })
+//   console.error('Unable to connect to the database:', error.message)
+}
+})
 
 app.use(dashboardRouter)
 app.use(storeInformationRouter)
